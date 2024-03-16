a50774ec0a5e633027d6f28ee5d0bc5272435c92
phunghaphung <haphungworks@gmail.com>
2024-03-16 14:59:09 -0400

    fix scripts

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -17,11 +21,15 @@ app.get("/home", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 5000;
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (res, req) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
